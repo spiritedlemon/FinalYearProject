376d5d68f24e36e1335adca6989f15f4245f66d0
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2019-02-19 12:27:27 +0000

    Adjusted rigidbody of levers and control box to stop forklift exploding & started grab scripts for wheel and lever

---
+++ ForkliftOperatingSimulator/Assets/Scripts/SteeringWheel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringWheel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Set limit for rotation
+		//Rotate with hand movement if grabbed == true
+        
+    }
+	
+	void OnCollisionStay(Collision CollisionInfo)
+	{
+		if(CollisionInfo.gameObject.tag == "Hand")
+		{
+			Debug.Log("grabbable");
+			//if trigger pressed set grabbed to true
+			
+		}
+	}
+}
