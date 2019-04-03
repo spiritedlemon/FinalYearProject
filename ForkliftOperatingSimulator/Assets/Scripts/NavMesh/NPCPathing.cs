253be1e9453651d2cb853e1ccff0c83473694407
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2019-04-03 20:11:03 +0100

    Created basic pathing script

--- ForkliftOperatingSimulator/Assets/Scripts/NavMesh/NPCPathing.cs
+++ ForkliftOperatingSimulator/Assets/Scripts/NavMesh/NPCPathing.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NPCPathing : MonoBehaviour
+public class NPCMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+    // Use this for initialization
     void Start()
     {
-        
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetDestination()
     {
-        
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
     }
 }
